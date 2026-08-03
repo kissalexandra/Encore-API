a0f789e98623ec9594f8c8f5829c269fc31fbbd1
Alexandra Kiss <ops@alexandra.lu>
2026-08-03 16:42:44 +0200

    Add initializer

--- Sources/EncoreApi/Models/DiscordApplication.swift
+++ Sources/EncoreApi/Models/DiscordApplication.swift
@@ -8,7 +8,7 @@
 import Fluent
 import Vapor
 
-internal final class DiscordApplication: Model, @unchecked Sendable {
+internal final class DiscordApplication: Model, Content, @unchecked Sendable {
     internal static let schema: String = "discord_applications"
 
     @ID(key: .id)
@@ -16,4 +16,10 @@ internal final class DiscordApplication: Model, @unchecked Sendable {
 
     @Field(key: "application_identifier")
     internal var applicationIdentifier: String
+
+    init() {}
+
+    init(applicationIdentifier: String) {
+        self.applicationIdentifier = applicationIdentifier
+    }
 }
