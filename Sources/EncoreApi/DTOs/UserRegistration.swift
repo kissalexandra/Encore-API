a69a9b52830d9f947b1840d179ba98420d525689
Alexandra Kiss <ops@alexandra.lu>
2026-08-06 19:13:55 +0200

    Add validation error messages

--- Sources/EncoreApi/DTOs/UserRegistration.swift
+++ Sources/EncoreApi/DTOs/UserRegistration.swift
@@ -14,7 +14,7 @@ internal struct UserRegistration: Content {
 
 extension UserRegistration: Validatable {
     internal static func validations(_ validations: inout Validations) {
-        validations.add("username", as: String.self, is: !.empty)
-        validations.add("password", as: String.self, is: .count(8...))
+        validations.add("username", as: String.self, is: !.empty, customFailureDescription: "Your username can't be empty.")
+        validations.add("password", as: String.self, is: .count(8...), customFailureDescription: "Your password has to be at least 8 characters.")
     }
 }
